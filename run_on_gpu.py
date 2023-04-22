aafc9f97c746d4ee3055551141baef6e5818b499
Daniel Harper <dghjunior@gmail.com>
2023-04-22 16:44:16 -0400

    Updated for running yolo

--- run_on_gpu.py
+++ run_on_gpu.py
@@ -3,6 +3,6 @@ import os
 
 @jit(target_backend='cuda')
 def yolo():
-    os.system('python yolo/train.py --workers 1 --device 0 --batch-size 16 --epochs 100 --img 640 640 --hyp yolo/data/hyp.scratch.custom.yaml --name yolov7-custom --weights yolo/yolov7.pt')
+    os.system('python yolo/train.py --workers 1 --device 0 --batch-size 8 --epochs 100 --img 640 640 --hyp yolo/data/hyp.scratch.custom.yaml --name yolov7-custom --weights yolo/yolov7.pt')
 
 yolo()
