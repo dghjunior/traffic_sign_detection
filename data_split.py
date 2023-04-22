5e9a82a318cc3f24ccedc1ee3946513cb876790e
Daniel Harper <dghjunior@gmail.com>
2023-04-22 15:36:18 -0400

    Create data split file

---
+++ data_split.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+from sklearn.model_selection import train_test_split
+
+def split_images():
+    data = os.listdir('archive/images')
+    train, test = train_test_split(data, test_size=0.2, random_state=27)
+
+    for file in train:
+        shutil.move('archive/images/' + file, 'archive/train/' + file)
+        shutil.move('archive/labels/' + file[:-3] + 'txt', 'archive/train/' + file)
+
+    for file in test:
+        shutil.move('archive/images/' + file, 'archive/test/' + file)
+        shutil.move('archive/labels/' + file[:-3] + 'txt', 'archive/test/' + file)
+
+def split_labels():
+    data = os.listdir('archive/train')
+
+    for img in data:
+        label = 'archive/labels/' + img[:-3] + 'txt'
+        print(label)
+
+split_images()
