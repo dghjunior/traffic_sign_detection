56734a19a4f85896916bb97bec47619b0685b70a
Daniel Harper <dghjunior@gmail.com>
2023-05-01 03:47:00 -0400

    Ran SVM data splitting script

--- data/preprocessing/svm_splitting.py
+++ data/preprocessing/svm_splitting.py
@@ -1,20 +1,20 @@
 from PIL import Image
+from bs4 import BeautifulSoup
 
 for i in range(877):
     im = Image.open("data/images/road" + str(i) + ".png")
-    f = open('data/yolo_annotations/road' + str(i) + '.txt', 'r')
-    box = f.readline()
-    count = 1
-    while box != '':
-        box = box.split(' ')
-        x = int(box[1])
-        y = int(box[2])
-        w = int(box[3])
-        h = int(box[4])
-        x1 = int(x - w/2)
-        y1 = int(y - h/2)
-        x2 = int(x + w/2)
-        y2 = int(y + h/2)
-        im1 = im.crop((x1, y1, x2, y2))
-        im1.save("data/cropped_images/road" + str(i) + '_' + box[0] + '-' + str(count) + ".png")
-        box = f.readline()
+    f = open('data/voc_annotations/road' + str(i) + '.xml', 'r')
+    bs_data = BeautifulSoup(f.read(), 'lxml')
+
+    objects = bs_data.find_all('object')
+    count = 0
+    for obj in objects:
+        count += 1
+        category = obj.find('name').text
+        bndbox = obj.find('bndbox')
+        xmin = int(bndbox.find('xmin').text)
+        ymin = int(bndbox.find('ymin').text)
+        xmax = int(bndbox.find('xmax').text)
+        ymax = int(bndbox.find('ymax').text)
+        im_crop = im.crop((xmin, ymin, xmax, ymax))
+        im_crop.save('svm/data/cropped_images/' + category + '/road' + str(i) + '_' + str(count) + '.png')
