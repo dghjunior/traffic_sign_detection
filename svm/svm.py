af1b1e0242fbcba2eec498450f4dd27f0de49fec
Daniel Harper <dghjunior@gmail.com>
2023-05-01 04:06:28 -0400

    based on kaggle SVM. Trying new method soon

--- svm/svm.py
+++ svm/svm.py
@@ -2,7 +2,39 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
+from keras import Sequential
+from keras.layers import Conv2D,Dense, MaxPool2D, Flatten
+from keras.regularizers import l2
 import os
 
 train_dir = 'svm/data/train'
-val_dir = 'svm/data/val'
+test_df = 'svm/data/test'
+
+train_datagen = ImageDataGenerator(rescale=(1/255.),shear_range = 0.2,zoom_range=0.2,
+                                   horizontal_flip=True)
+training_set = train_datagen.flow_from_directory(directory = train_dir,target_size=(64,64),
+                                                batch_size=32,
+                                                class_mode = "binary")
+test_datagen = ImageDataGenerator(rescale=(1/255.))
+test_set = test_datagen.flow_from_directory(directory = test_df,target_size=(64,64),
+                                                batch_size=32,
+                                                class_mode = "binary")
+
+
+
+model = Sequential()
+model.add(Conv2D(filters = 32, padding = "same",activation = "relu",kernel_size=3, strides = 2,input_shape=(64,64,3)))
+model.add(MaxPool2D(pool_size=(2,2),strides = 2))
+
+model.add(Conv2D(filters = 32, padding = "same",activation = "relu",kernel_size=3))
+model.add(MaxPool2D(pool_size=(2,2),strides = 2))
+
+model.add(Flatten())
+model.add(Dense(128,activation="relu"))
+
+number_of_classes = 4
+
+model.add(Dense(number_of_classes,kernel_regularizer = l2(0.01),activation= "softmax"))
+model.compile(optimizer="adam",loss="squared_hinge", metrics = ['accuracy'])
+
+history = model.fit(training_set,validation_data=test_set,epochs=15)
