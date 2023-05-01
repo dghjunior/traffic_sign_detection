d816cea852e83cfd58736ac530c623ab248a1dfc
Daniel Harper <dghjunior@gmail.com>
2023-05-01 03:54:31 -0400

    Reorganizing data for SVM

---
+++ svm/data/train_test_split.py
@@ -0,0 +1,42 @@
+import shutil
+import os
+import numpy as np
+import argparse
+
+def get_files_from_folder(path):
+
+    files = os.listdir(path)
+    return np.asarray(files)
+
+def main(path_to_data, path_to_test_data, train_ratio):
+    # get dirs
+    _, dirs, _ = next(os.walk(path_to_data))
+
+    # calculates how many train data per class
+    data_counter_per_class = np.zeros((len(dirs)))
+    for i in range(len(dirs)):
+        path = os.path.join(path_to_data, dirs[i])
+        files = get_files_from_folder(path)
+        data_counter_per_class[i] = len(files)
+    test_counter = np.round(data_counter_per_class * (1 - train_ratio))
+
+    # transfers files
+    for i in range(len(dirs)):
+        path_to_original = os.path.join(path_to_data, dirs[i])
+        path_to_save = os.path.join(path_to_test_data, dirs[i])
+
+        #creates dir
+        if not os.path.exists(path_to_save):
+            os.makedirs(path_to_save)
+        files = get_files_from_folder(path_to_original)
+        # moves data
+        for j in range(int(test_counter[i])):
+            dst = os.path.join(path_to_save, files[j])
+            src = os.path.join(path_to_original, files[j])
+            shutil.move(src, dst)
+
+if __name__ == "__main__":
+    data_path = 'svm/data/cropped_images'
+    test_data_path_to_save = 'svm/data/test'
+    train_ratio = 0.8
+    main(data_path, test_data_path_to_save, train_ratio)
