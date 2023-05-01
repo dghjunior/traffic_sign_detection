15ea662fc78c992a49491ae255ddfee7c2fa7ff2
Daniel Harper <dghjunior@gmail.com>
2023-05-01 03:10:27 -0400

    Working on dataset formatting for SVM

---
+++ svm/svm.py
@@ -0,0 +1,8 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+import os
+
+train_dir = 'svm/data/train'
+val_dir = 'svm/data/val'
