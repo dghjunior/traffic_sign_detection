808eb5bde19590ad5f4bcd78fdd79dcc2e480d38
Daniel Harper <dghjunior@gmail.com>
2023-05-02 03:20:41 -0400

    Split and format Caffe data

--- caffe/data/caffe_split.py
+++ caffe/data/caffe_split.py
@@ -10,21 +10,25 @@ def reorganize():
     with open("caffe/data/train.txt", "w") as f:
         for clazz in classes:
             for image in os.listdir("caffe/data/train/" + clazz):
-                os.rename("caffe/data/train/" + clazz + '/' + image, "caffe/data/train/" + classes.index(clazz) + '-' + image)
+                os.rename("caffe/data/train/" + clazz + '/' + image, "caffe/data/train/" + str(classes.index(clazz)) + '-' + image)
                 f.write(image + ' ' + str(classes.index(clazz)) + '\n')
 
 def split():
     # split the data into train and test sets
-    train_images, test_images= train_test_split('caffe/data/train', test_size=0.2, random_state=32)
+    train_images, test_images= train_test_split(os.listdir('caffe/data/train/'), test_size=0.2, random_state=32)
 
-    with open('caffe/data/train.txt', 'r') as f:
+    with open('caffe/data/train.txt', 'w') as f:
         for image in train_images:
-            print()
+            clazz = image.split('-')[0]
+            name = image.split('-')[1]
+            f.write(name + ' ' + clazz + '\n')
 
     # move the test images and labels into the test folder
     with open("caffe/data/test.txt", "w") as f:
         for image in test_images:
+            clazz = image.split('-')[0]
+            name = image.split('-')[1]
+            f.write(name + ' ' + clazz + '\n')
             os.rename("caffe/data/train/" + image, "caffe/data/test/" + image)
-            f.write(image + '\n')
 
-reorganize()
+split()
