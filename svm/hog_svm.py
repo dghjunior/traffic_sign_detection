3a5d4f13af52e15f98b0d592e1d46cbc564e2aa3
Daniel Harper <dghjunior@gmail.com>
2023-05-03 00:19:32 -0400

    Run of SVM with HOG

---
+++ svm/hog_svm.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import os
+from skimage.transform import resize
+from skimage.io import imread
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearnex import patch_sklearn
+patch_sklearn()
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from skimage.feature import hog
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image
+
+Categories=["trafficlight", "speedlimit", "crosswalk", "stop"]
+flat_data_arr=[] #input array
+target_arr=[] #output array
+datadir='svm/data/train/'
+#path which contains all the categories of images
+for i in Categories:
+	
+	print(f'loading... category : {i}')
+	path=os.path.join(datadir,i)
+	for img in os.listdir(path):
+		img_array=imread(os.path.join(path,img))
+		img_resized=resize(img_array,(150,150,3))
+		img = Image.open(os.path.join(path,img))
+		img = img.resize((150,150))
+		gray = img.convert('L')
+		fd = hog(gray, orientations=9, pixels_per_cell=(16, 16), cells_per_block=(4, 4), visualize=False)
+		flat_data_arr.append(fd)
+		target_arr.append(Categories.index(i))
+	print(f'loaded category:{i} successfully')
+flat_data=np.array(flat_data_arr)
+target=np.array(target_arr)
+
+#dataframe
+df=pd.DataFrame(flat_data)
+df['Target']=target
+df.shape
+
+#input data
+x=df.iloc[:,:-1]
+#output data
+y=df.iloc[:,-1]
+
+# Splitting the data into training and testing sets
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,
+											   random_state=77,
+											   stratify=y)
+
+# Defining the parameters grid for GridSearchCV
+param_grid={'C':[0.1,1,10,100],
+			'gamma':[0.0001,0.001,0.1,1],
+			'kernel':['rbf','poly']}
+
+# Creating a support vector classifier
+svc=svm.SVC(probability=True)
+
+# Creating a model using GridSearchCV with the parameters grid
+model=GridSearchCV(svc,param_grid)
+
+# Training the model using the training data
+history = model.fit(x_train,y_train)
+
+# Testing the model using the testing data
+y_pred = model.predict(x_test)
+
+# Calculating the accuracy of the model
+accuracy = accuracy_score(y_pred, y_test)
+
+# Print the accuracy of the model
+print(f"The model is {accuracy*100}% accurate")
+
+clf_report = classification_report(y_test, y_pred, target_names=["trafficlight", "speedlimit", "crosswalk", "stop"], output_dict=True)
+sns.heatmap(pd.DataFrame(clf_report).iloc[:-1, :].T, annot=True)
+plt.show()
+
+# Saving the model
+pickle.dump(model, open('svm/model/svm_hog_model.pkl','wb'))
